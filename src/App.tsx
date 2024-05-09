1abb37256a3300f9a7b8980ba07d62dc5dadc78c
Bin Li <bin.li@genesys.com>
2024-05-09 21:09:02 +1000

    init: basic version of react_bpmn_designer

--- src/App.tsx
+++ src/App.tsx
@@ -2,129 +2,31 @@ import { useState } from "react";
 import "./App.css";
 
 import BpmnJsReact from "./lib/BpmnJsReact";
-import { defaultBpmnXml } from "./utils/bpmn.utils";
+import BpmnDesigner from "./components/BpmnDesigner";
 import { useBpmnJsReact } from "./hooks/bpmn.hook";
 
-// import { Stack, ActionIcon, MantineProvider, Button } from "@mantine/core";
-// import { IconZoomIn, IconZoomOut } from "@tabler/icons";
+import {Spin} from "antd";
 
 function App() {
-  //const ref = useRef<BpmnJsReactHandle>(null);
+  const { data, setData } = useState<string>("");
+  const { currentType, setCurrentType } = useState<string>("add");
+  const { spinning, setSpinning } = useState<boolean>(true);
 
   const [elements, setElements] = useState<any>([]);
 
   const bpmnReactJs = useBpmnJsReact();
 
   return (
-      <div className="">
-    <div className="App" style={{height:"500px"}}>
-      <BpmnJsReact
-        mode="edit"
-        useBpmnJsReact={bpmnReactJs}
-        // ref={ref}
-        click={(e: any) => setElements([e.element])}
-      ></BpmnJsReact>
-      <button
-        onClick={async () => {
-          const result = await bpmnReactJs.saveXml({
-            format: true,
-          });
-
-          // console.log(result);
-
-          console.log(result.xml);
-
-          // console.log(await bpmnReactJs.saveXmLAsync());
-          // console.log(
-          //   ref.current?.saveXml((err: any, xml: string) => console.log(xml))
-          // );
-        }}
-      >
-        save
-      </button>
-      <button
-        onClick={() => {
-          console.log(elements);
-          // console.log(
-          //   bpmnReactJs.setColor(elements, {
-          //     stroke: "#00ff00",
-          //     fill: "#ffff00",
-          //   })
-          // );
-        }}
-      >
-        set color
-      </button>
-      <button
-        onClick={() => {
-          console.log(
-            bpmnReactJs.setColor(elements, {
-              stroke: "black",
-              fill: "white",
-            })
-          );
-        }}
-      >
-        clear color
-      </button>
-      <button
-        onClick={() => {
-          // elements.forEach((element: any) => {
-          //    ref.current?.addMarker(element.id, "highlight");
-          // });
-        }}
-      >
-        addMarker
-      </button>
-      <button
-        onClick={() => {
-          elements.forEach((element: any) => {
-            // ref.current?.removeMarker(element.id, "highlight");
-          });
-        }}
-      >
-        removeMarker
-      </button>
-      <button
-        onClick={() => {
-          bpmnReactJs.setAttribute(elements[0]?.id, "property", JSON.stringify({ test: "test" }));
-          bpmnReactJs.setAttribute(elements[0]?.id, "settings", JSON.stringify({ time: true }));
-        }}
-      >
-        setAttribute
-      </button>
-      <button
-        onClick={() => {
-          const attr = bpmnReactJs.getAttribute(elements[0]?.id, "property");
-
-          console.log(attr);
-        }}
-      >
-        getAttribute
-      </button>
-      <button
-        onClick={() => {
-          const incoming = bpmnReactJs.getIncoming(elements[0]?.id);
-
-          console.log(incoming);
-        }}
-      >
-        get Incoming
-      </button>
-      <button
-        onClick={() => {
-          const outgoing = bpmnReactJs.getOutgoing(elements[0]?.id);
-
-          console.log(outgoing);
-        }}
-      >
-        get outgoing
-      </button>
-      <br />
-
-      {/*<BpmnJsReact useBpmnJsReact={bpmnReactJs} xml={defaultBpmnXml} zoomActions={false}></BpmnJsReact>*/}
-    </div>
-      </div>
+   <Spin spinning={spinning}>
+       <div style={{width: "100%", height: "92vh"}}>
+           {/*<BpmnJsReact*/}
+           {/*    mode="edit"*/}
+           {/*    useBpmnJsReact={bpmnReactJs}*/}
+           {/*    click={(e: any) => setElements([e.element])}*/}
+           {/*></BpmnJsReact>*/}
+           <BpmnDesigner xml={data} type={currentType} />
+       </div>
+   </Spin>
   );
 }
 
---
+++ src/components/BpmnDesigner/index.tsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import "bpmn-js/dist/assets/diagram-js.css";
+import "bpmn-js/dist/assets/bpmn-font/css/bpmn.css";
+import "bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css";
+import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
+import "./bpmn-designer.less";
+import Modeler from "bpmn-js/lib/Modeler";
+
+import flowableModdleDescriptor from "./flow.json";
+export default function index(props:any) {
+    const { xml, type, modelId } = props;
+    const [bpmnInstance, setBpmnInstance] = useState({});
+
+    useEffect(() => {
+        const bpmnModeler = new Modeler({
+            container: "#flowCanvas",
+            moddleExtensions: {
+                flowable: flowableModdleDescriptor, //添加flowable前缀
+            },
+        });
+
+        // 注册bpmn实例
+        const instance = {
+            modeler: bpmnModeler,
+            modeling: bpmnModeler.get("modeling"),
+            moddle: bpmnModeler.get("moddle"),
+            eventBus: bpmnModeler.get("eventBus"),
+            bpmnFactory: bpmnModeler.get("bpmnFactory"),
+            elementRegistry: bpmnModeler.get("elementRegistry"),
+            replace: bpmnModeler.get("replace"),
+            selection: bpmnModeler.get("selection"),
+        };
+        setBpmnInstance(instance);
+        getActiveElement(instance);
+        //if (type === "add") bpmnModeler.importXML(initXml);
+
+        // 修改节点hover时的背景色
+        const container = document.getElementsByClassName("djs-container")[0];
+        container.style.setProperty(
+            "--shape-drop-allowed-fill-color",
+            "transparent"
+        );
+    }, []);
+
+    function getActiveElement(instance:any) {
+        const { modeler } = instance;
+        // 初始第一个选中元素 bpmn:Process
+        initFormOnChanged(null, instance);
+        modeler.on("import.done", (e:any) => {
+            initFormOnChanged(null, instance);
+        });
+        // 监听选择事件，修改当前激活的元素以及表单
+        modeler.on("selection.changed", ({ newSelection }: { newSelection: any[] }) => {
+            initFormOnChanged(newSelection[0] || null, instance);
+        });
+    }
+
+    function initFormOnChanged(element:any, instance:any) {
+        let activatedElement = element;
+        const elementRegistry = instance.modeler.get("elementRegistry");
+        if (!activatedElement) {
+            activatedElement =
+                elementRegistry.find((el:any) => el.type === "bpmn:Process") ||
+                elementRegistry.find((el:any) => el.type === "bpmn:Collaboration");
+        }
+        if (!activatedElement) return;
+        // console.log(`
+        //           ----------
+        //   select element changed:
+        //             id:  ${activatedElement.id}
+        //           type:  ${activatedElement.businessObject.$type}
+        //           ----------
+        //           `);
+        console.log("businessObject: ", activatedElement.businessObject);
+        setBpmnInstance({ bpmnElement: activatedElement, ...instance });
+    }
+
+    return (
+        <div className="bpmn-designer">
+            <div>
+                <div id="flowCanvas" className="flow-canvas"></div>
+            </div>
+
+        </div>
+    );
+}
